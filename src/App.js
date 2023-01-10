ec3fa796f04e3020e8064f0d7e15f305b4af3ea7
yyellen <yyopenfolder@gmail.com>
2023-01-10 16:43:09 +0800

    install tailwind and setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import CustomComponent from './CustomComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomComponent>
+        <div>
+          <h1 className='text-3xl text-blue-600'>Title</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, in.</p>
+        </div>
+      </CustomComponent>
     </div>
   );
 }
---
+++ src/CustomComponent/index.js
@@ -0,0 +1,8 @@
+const CustomComponent = (props) => {
+ const { children } = props
+  return (
+    <div>{ children }</div>
+  )
+}
+
+export default CustomComponent;
